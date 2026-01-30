25421d30a6ce108a9a3ab0b8c4e94d29193ffe9c
Dan <35927536+dan-mba@users.noreply.github.com>
2026-01-30 06:19:44 -0500

    change proxy

--- src/util/Endpoints.js
+++ src/util/Endpoints.js
@@ -5,5 +5,5 @@
 export const endpoint = "https://api.nhtsa.gov/products/vehicle";
 export const recallEndpoint = "https://api.nhtsa.gov/recalls/recallsByVehicle";
 export const datatype = "issueType=r";
-const proxy = 'https://corsproxy.io/?url=';
+const proxy = 'https://api.cors.lol/?url=';
 export const proxyFetch = (api) => fetch(proxy + encodeURIComponent(api)).then(res => res.json());

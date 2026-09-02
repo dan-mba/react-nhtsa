d766759a46a6f73e40a0851e7eea2eb1db3fad45
Dan <35927536+dan-mba@users.noreply.github.com>
2026-09-02 06:25:25 -0400

    switch to native react compiler

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import babel from '@rolldown/plugin-babel';
 import {createHtmlPlugin} from 'vite-plugin-html';
 
 export default defineConfig({
   plugins: [
-    react(),
-    babel({
-      presets: [reactCompilerPreset()]
-    }),
+    react({ compiler: true }),
     createHtmlPlugin()
   ],
   server: {
